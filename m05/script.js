d9f169e5b24233c133d8396122d81340ec797ec9
guentherp0 <penton.guenther@bellevuecollege.edu>
2024-10-25 17:33:01 -0700

    Update script.js

--- m05/script.js
+++ m05/script.js
@@ -1,20 +1,11 @@
 var table = prompt("Enter a number");             // Unit of table
-var operator = 'multiplication'; // Type of calculation
 var i = 1;                 // Set counter to 1
 var msg = '<h2>Multiplication Table</h2>';              // Message
 
-if (operator === 'addition') {
-  // Do addition
-  while (i < 11) {
-    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
-    i++;
-  }
-} else {
-  // Do multiplication
-  while (i < 11) {
-    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
-    i++;
-  }
+// Do multiplication
+while (i < 11) {
+  msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+  i++;
 }
 
 // Write the message into the page
