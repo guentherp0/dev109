9da9616fbe45a482c2c008a5627f42ac7d54b20c
guentherp0 <penton.guenther@bellevuecollege.edu>
2024-10-30 20:15:23 -0700

    Create scripts.js

---
+++ a3/scripts.js
@@ -0,0 +1,6 @@
+function createRhombus() {
+      var height = parseInt(document.getElementById('height').value);
+      var oddColor = document.getElementById('odd-color').value;
+      var evenColor = document.getElementById('even-color').value;
+      var symbol = document.getElementById('symbol').value;
+      var rhombusContainer = document.getElementById('rhombus-output');
