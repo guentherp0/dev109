9456a20c385a82fa196ebc367280affc34121c2b
guentherp0 <penton.guenther@bellevuecollege.edu>
2024-10-31 20:42:05 -0700

    Update scripts.js

--- a3/scripts.js
+++ a3/scripts.js
@@ -1,16 +1,16 @@
 function createRhombus() {
-      var height = parseInt(document.getElementById('height').value) - 1;
+      var height = parseInt(document.getElementById('height').value);
       var oddColor = document.getElementById('odd-color').value;
       var evenColor = document.getElementById('even-color').value;
       var symbol = document.getElementById('symbol').value;
       var rhombusContainer = document.getElementById('rhombus-output');
 
       var txt = "";
-      for (let i = 0; i < height; i++) {
-            for (let j = 0; j < -(i - height); j++){
+      for (let i = 0; i <= height; i++) {
+            for (let j = 0; j <= -(i - height); j++){
                   txt += symcol(symbol, "coral");
             }
-            for (let j = 0; j < i; j++){
+            for (let j = 0; j <= i; j++){
                   txt += symcol(symbol, oddColor);
             }
             txt += "<br>"
