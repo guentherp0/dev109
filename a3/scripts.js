7d5a0b7240eb224cde83d4869a3b50baf547004e
guentherp0 <penton.guenther@bellevuecollege.edu>
2024-10-31 21:13:28 -0700

    Update scripts.js

--- a3/scripts.js
+++ a3/scripts.js
@@ -1,12 +1,12 @@
 function createRhombus() {
-      var height = parseInt(document.getElementById('height').value);
-      var oddColor = document.getElementById('odd-color').value;
-      var evenColor = document.getElementById('even-color').value;
-      var symbol = document.getElementById('symbol').value;
-      var rhombusContainer = document.getElementById('rhombus-output');
+      var height = parseInt(document.getElementById("height").value);
+      var oddColor = document.getElementById("odd-color").value;
+      var evenColor = document.getElementById("even-color").value;
+      var symbol = document.getElementById("symbol").value;
+      var rhombusContainer = document.getElementById("rhombus-output");
 
       var txt = "";
-      for (let i = 0; i < height; i++) {
+      for (let i = 0; i < height; i++){
             for (let j = 0; j < -(i - height + 1); j++){
                   txt += symcol(" ", "white");
             }
@@ -23,7 +23,7 @@ function createRhombus() {
                   txt += symcol(" ", "white");
             }
             txt += "<br>"
-      } 
+      }
       txt += "<br>"
       for (let i = height - 1; i >= 0; i--) {
             for (let j = 0; j < -(i - height + 1); j++){
@@ -42,7 +42,7 @@ function createRhombus() {
                   txt += symcol(" ", "white");
             }
             txt += "<br>"
-      } 
+      }
 
       rhombusContainer.innerHTML = txt;
 }
