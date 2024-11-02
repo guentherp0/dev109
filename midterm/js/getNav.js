49d5e35f97247a3c66345efa16c10eec0dffff53
guentherp0 <penton.guenther@bellevuecollege.edu>
2024-11-02 16:36:28 -0700

    Update getNav.js

--- midterm/js/getNav.js
+++ midterm/js/getNav.js
@@ -1,16 +1,15 @@
-function getNav(){
-  var nav_txt = `
-  <a href="index.html">Home</a>
-  <a href="about.html">About</a>
-  <a href="courses.html">Courses</a>
-  <a href="artifacts.html">Artifacts</a>
-  <a href="modules.html">Modules</a>
-  <a href="contact.html">Contact</a>
-  <a href="creative.html">Occupy</a>
-  <a href="validated.html">Validated</a>
-  `;
-  
-  // finds the nav div
-  var nav = document.getElementById('nav');
-  nav.innerHTML = nav_txt;
-}
+// the navigation bar
+var nav_txt = `
+<a href="index.html">Home</a>
+<a href="about.html">About</a>
+<a href="courses.html">Courses</a>
+<a href="artifacts.html">Artifacts</a>
+<a href="modules.html">Modules</a>
+<a href="contact.html">Contact</a>
+<a href="creative.html">Occupy</a>
+<a href="validated.html">Validated</a>
+`;
+
+// finds the nav div
+var nav = document.getElementById('nav');
+nav.innerHTML = nav_txt;
