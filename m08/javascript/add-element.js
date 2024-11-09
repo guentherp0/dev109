bbe24eb2be70d1cf1e44a0851eb2449838b53b10
guentherp0 <penton.guenther@bellevuecollege.edu>
2024-11-09 15:33:17 -0800

    Update add-element.js

--- m08/javascript/add-element.js
+++ m08/javascript/add-element.js
@@ -12,3 +12,22 @@ var position = document.getElementsByTagName('ul')[0];
 
 // Insert the new element into its position.
 position.appendChild(newEl);
+
+// Function to add a new item to the list
+function addItem() {
+    // Get the text from the input box
+    var itemText = document.getElementById('itemInput').value;
+    
+    // If there's text, add it to the list
+    if (itemText.trim() !== "") {
+        // Create a new list item (li)
+        var newItem = document.createElement('li');
+        newItem.textContent = itemText;
+
+        // Add the new item to the list (ul)
+        document.getElementById('todo').appendChild(newItem);
+
+        // Clear the text box after adding the item
+        document.getElementById('itemInput').value = "";
+    }
+}
