64e1cf96f7d038539b6571cceb9efbd7102c59fd
guentherp0 <penton.guenther@bellevuecollege.edu>
2024-11-16 18:48:16 -0800

    Update validateForm.js

--- midterm/js/validateForm.js
+++ midterm/js/validateForm.js
@@ -10,5 +10,27 @@ function validateForm(){
     validFirstname = true;
   }
   
+  var validLastname=false;
+  var firstname = document.getElementById("lastname").value;
+  if (lastname==="null" || lastname==="" || lastname.length > 50) {
+    errorMessages += "<p class=\"error\">The lastname is required and cannot be greater than 50 characters</p>";
+  }
+  else {
+    validLastname = true;
+  }
+  
+  var validEmail=false;
+  var email = document.getElementById("email").value;
+  var atpos = email.indexOf("@");
+  var dotpos = email.lastIndexOf(".");
+  if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=email.length) {
+    errorMessages = "<p>Invalid email</p>";
+  }
+  else {
+    validEmail = true;
+  }
+
+
+  
   document.getElementById("errorMessages").innerHTML = errorMessages;
 }
