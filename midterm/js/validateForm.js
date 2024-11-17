235d139c4595d597bc59a1b65eda1e30542c0127
guentherp0 <penton.guenther@bellevuecollege.edu>
2024-11-16 18:29:33 -0800

    Update validateForm.js

--- midterm/js/validateForm.js
+++ midterm/js/validateForm.js
@@ -1,15 +1,15 @@
 function validateForm(){
-  var n = document.getElementById('name').value;
-//1) create a variable to control status of each field. Assume that they are not valid
-
-//2) create variables to read the values from html text inputs
-
-//3) do the validation
-
-//4) send error messages 
-
-//5) return the status of each field
   var errorMessages = "";
-  errorMessages += "<p class=\"error\">" + n + "</p>";
+  
+  var validFirstname=false;
+  var firstname = document.getElementById("firstname").value;
+  if (firstname==="null" || firstname==="" || firstname.length > 20) {
+    errorMessages += "<p>The firstname is required and cannot be greater than 20 characters</p>";
+  }
+  else {
+    validFirstname = true;
+  }
+    
+  errorMessages += "<p class=\"error\">" + errorMessages + "</p>";
   document.getElementById("errorMessages").innerHTML = errorMessages;
 }
