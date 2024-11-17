04660640f2bef4ea8de2a0c762501abbe331c02d
guentherp0 <penton.guenther@bellevuecollege.edu>
2024-11-16 18:08:40 -0800

    Update validateForm.js

--- midterm/js/validateForm.js
+++ midterm/js/validateForm.js
@@ -1,1 +1,14 @@
+function validateForm(){
+//1) create a variable to control status of each field. Assume that they are not valid
 
+//2) create variables to read the values from html text inputs
+
+//3) do the validation
+
+//4) send error messages 
+
+//5) return the status of each field
+  var errorMessages = "";
+  errorMessages += "<p>:(</p>"
+  document.getElementById("errorMessages").innerHTML = errorMessages;
+}
