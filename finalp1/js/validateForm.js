09365a0a4886742b439fcf99614d5061d1631a2e
guentherp0 <penton.guenther@bellevuecollege.edu>
2024-12-03 19:37:44 -0800

    Update validateForm.js

--- finalp1/js/validateForm.js
+++ finalp1/js/validateForm.js
@@ -1,4 +1,6 @@
+// the general function that the submit button on "contact.html" calls
 function validateForm(){
+	// get an error checker/display variable for all entry boxes
   var errorFirstname = "";
   var errorLastname = "";
   var errorEmail = "";
@@ -31,7 +33,7 @@ function validateForm(){
   var email = document.getElementById("email").value;
   var atpos = email.indexOf("@");
   var dotpos = email.lastIndexOf(".");
-  if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=email.length) {
+  if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=email.length) { // makes sure there's an atsign, a period after the atsign, and two characters after the period
     errorEmail += "<p class=\"error\">Invalid email</p>";
   }
   else {
@@ -40,7 +42,7 @@ function validateForm(){
   
   var validPhone=false;
   var phone = document.getElementById("phone").value;
-  if (isNaN(phone) || phone.length > 15 || phone===null || phone==="") {
+  if (isNaN(phone) || phone.length > 15 || phone===null || phone==="") { // makes sure it's a number smaller than 15 digits
     errorPhone += "<p class=\"error\">Invalid phone number</p>";
   }
   else {
@@ -85,21 +87,22 @@ function validateForm(){
 
   var state = document.getElementById("state").value;
   var validState=true;
-  // no need to verify
+  // no need to verify, it's a dropdown box with a valid item selected by default
   
   var country = document.getElementById("country").value;
   var validCountry=true;
-  // no need to verify
+  // no need to verify, it's a dropdown box with a valid item selected by default
   
   var validZipcode=false;
   var zipcode = document.getElementById("zipcode").value;
-  if (country === "usa" && zipcode.length > 5) {
+  if (country === "usa" && zipcode.length > 5) { // all non-usa zipcodes are accepted by default, usa zipcodes must be less than or equal to 5 digits
     errorZip += "<p class=\"error\">The zipcode is invalid</p>";
   }
   else {
     validZipcode = true;
   }
 
+	// if the entry passed ALL validations
   if (validFirstname && validLastname && validEmail && validPhone && validUsername && validPassword && validAddress && validCity && validState && validCountry && validZipcode){
     const params = new URLSearchParams({
       firstname: firstname,
@@ -114,11 +117,11 @@ function validateForm(){
       country: country,
       zipcode: zipcode,
       message: document.getElementById("message").value,
-    }).toString();
+    }).toString(); // gets the info to pass to the site
 
     const url = `thankyou.html?${params}`;
 
-    window.open(url, '_self');
+    window.open(url, '_self'); // sends the user to "thankyou.html"
   }
   document.getElementById("errorFirstname").innerHTML = errorFirstname;
   document.getElementById("errorLastname").innerHTML = errorLastname;
@@ -129,4 +132,5 @@ function validateForm(){
   document.getElementById("errorAddress").innerHTML = errorAddress;
   document.getElementById("errorCity").innerHTML = errorCity;
   document.getElementById("errorZip").innerHTML = errorZip;
+  // sets all the errors to show what went wrong to the user
 }
